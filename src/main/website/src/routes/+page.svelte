3d1ebc56b103aec01b8d2b3f74662603c068b897
Nocturnz <45483733+Nocturnz@users.noreply.github.com>
2024-02-15 04:26:52 -0500

    updated hosting location

--- src/main/website/src/routes/+page.svelte
+++ src/main/website/src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import { STServer } from '$lib/client/speedtest_browser';
 
 	const servers = [
-		new STServer('https://yeen.host:5202', 'Homelab - Nocturn Home'),
+		new STServer('https://yeen.host:5202', 'Homelab')
 	];
 </script>
 
